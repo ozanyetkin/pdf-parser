98786c9cd9145d712ad8a69ee7254cd9ed25a91e
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2022-04-30 14:13:23 +0300

    parser finished, output files deleted

--- pdf_parser.py
+++ pdf_parser.py
@@ -2,19 +2,20 @@ import os
 import fitz
 import textract
 import re
-import shutil
 import pandas as pd
 from glob import glob
 
 files = []
 start_dir = "/mnt/c/Users/Yetkin/Downloads/ARCH 282 All Sections-Mid Term Assignment-70029"
 pattern   = "*.pdf"
-df = pd.DataFrame(columns=["name", "page_count", "image_count", "word_count"])
+df = pd.DataFrame(columns=["name", "page_count", "image_count", "word_count", "cities"])
+cities_list = pd.read_csv("world-cities.csv")["name"].to_list()
 
 for dir,_,_ in os.walk(start_dir):
     files.extend(glob(os.path.join(dir,pattern)))
 
 for i, file in enumerate(files):
+    print(i)
     pdf_file = fitz.open(file)
     student_name = file.split("/")[-2].split("_")[0]
     page_count = len(pdf_file)
@@ -29,10 +30,14 @@ for i, file in enumerate(files):
     text = textract.process(file).decode('utf-8')
     words = re.findall(r"[^\W_]+", text, re.MULTILINE)
     word_count = len(words)
+    found_cities = set()
+    for word in words:
+        for city in cities_list:
+            if word.strip().lower() == city.lower():
+                found_cities.add(word)
 
-    # print(student_name, f"page count: {page_count}", f"image count: {image_count}", f"word count: {word_count}")
+    df.loc[i] = [student_name, page_count, image_count, word_count, str(found_cities).replace("{", "").replace("}", "")]
 
-    df.loc[i] = [student_name, page_count, image_count, word_count]
     pdf_file.close()
 
 print(df)
